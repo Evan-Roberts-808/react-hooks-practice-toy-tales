b1d000c4bfa3fb16afdb2a24558a12c95ead9d35
Evan <eroberts1998@icloud.com>
2023-05-04 16:17:54 -0400

    complete lab 33m

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({toys, onDelete, onLike}) {
+  const toysList = toys.map((toy) => {
+    return <ToyCard key={toy.id} toy={toy} onDelete={onDelete} onLike={onLike}/>
+  })
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{toysList}</div>
   );
 }
 
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,22 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToyForm() {
+function ToyForm({ onNewToy }) {
+
+  const [formData, setFormData] = useState({
+    name: "",
+    image: "",
+    likes: 0
+  })
+
+  function handleChange(event) {
+    setFormData({...formData, [event.target.name] : event.target.value})
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    fetch('http://localhost:3001/toys', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'name': formData.name,
+        'image': formData.image,
+        'likes': formData.likes
+      })
+    })
+    .then(response => response.json())
+    .then(newToy => {
+      onNewToy(newToy)
+    })
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
+          value={formData.name}
           placeholder="Enter a toy's name..."
           className="input-text"
+          onChange={handleChange}
         />
         <br />
         <input
           type="text"
           name="image"
+          value={formData.image}
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          onChange={handleChange}
         />
         <br />
         <input
