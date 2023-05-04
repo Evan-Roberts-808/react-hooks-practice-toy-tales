5d503126248a3d817885e8c15cee32bbc5811047
Evan <eroberts1998@icloud.com>
2023-05-04 13:22:24 -0400

    first attempt everything but the PATCH with buggy behavior

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({toys , onHandleDelete }) {
+  const toysList = toys.map((toy) => {
+      return <ToyCard onHandleDelete={() => onHandleDelete(toy.id) } key={toy.id} toy={toy}/>
+  })
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{toysList}</div>
   );
 }
 
